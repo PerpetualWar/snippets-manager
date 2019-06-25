504674f423feaf6fa8dd7dd6672413d991705fac
Srdjan Medjo <perpetualwarr@gmail.com>
2019-06-25 21:24:05 +0200

    window.opener prob on mobile...

--- src/App.js
+++ src/App.js
@@ -2,33 +2,46 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Storage from './util/storage';
 import { sendCode } from './services/api/login';
-import getQueryVariable from './util/getQueryVariable';
-import LoginPage from './components/LoginPage/LoginPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import Home from './pages/Home/Home';
+import getQueryVariable from './util/getQueryVariable';
 
+window.onload = async function(event) {
+  const code = getQueryVariable('code');
+
+  if (code) {
+    const { data } = await sendCode(code);
+    window.location.href = process.env.REACT_APP_URL;
+
+    if (data.includes('access_token')) {
+      const token = data.substring(data.indexOf('=') + 1, data.indexOf('&'));
+      Storage.set('access_token', token);
+    }
+  }
+};
 class App extends Component {
   state = {
     fetchingToken: false,
   };
 
   componentDidMount = () => {
-    window.addEventListener('message', async event => {
-      var msg = event.data;
-      console.log('msg :', msg);
-      if (msg.hasOwnProperty('code')) {
-        this.setState({ fetchingToken: true });
-        const { data } = await sendCode(msg.code);
-
-        if (data.includes('access_token')) {
-          const token = data.substring(
-            data.indexOf('=') + 1,
-            data.indexOf('&')
-          );
-          Storage.set('access_token', token);
-          this.setState({ fetchingToken: false });
-        }
-      }
-    });
+    // window.addEventListener('message', async event => {
+    //   var msg = event.data;
+    //   console.log('msg :', msg);
+    //   if (msg.hasOwnProperty('code')) {
+    //     this.setState({ fetchingToken: true });
+    //     const { data } = await sendCode(msg.code);
+    //     if (data.includes('access_token')) {
+    //       const token = data.substring(
+    //         data.indexOf('=') + 1,
+    //         data.indexOf('&')
+    //       );
+    //       Storage.set('access_token', token);
+    //       this.setState({ fetchingToken: false });
+    //       window.location.href = process.env.REACT_APP_URL;
+    //     }
+    //   }
+    // });
   };
 
   render() {
--- src/services/api/login.js
+++ src/services/api/login.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
 export const initiateLogin = () => {
-  window.open(
-    `${
-      process.env.REACT_APP_BASE_URL
-    }login/oauth/authorize?client_id=7c82a4326bf5e869b8bd&scope=gist,user:email`
-  );
+  return (window.location.href = `${
+    process.env.REACT_APP_BASE_URL
+  }login/oauth/authorize?client_id=7c82a4326bf5e869b8bd&scope=gist,user:email`);
+  // window.open(
+  //   `${
+  //     process.env.REACT_APP_BASE_URL
+  //   }login/oauth/authorize?client_id=7c82a4326bf5e869b8bd&scope=gist,user:email`
+  // );
 };
 
 export const sendCode = code =>
